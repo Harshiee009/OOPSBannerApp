9c1ce22626fa90da20370eeef299e0808f3ce846
Harshiee009 <hp9370@srmist.edu.in>
2026-03-09 23:00:07 +0530

    UC7

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,55 +1,82 @@
 public class OOPSBannerApp {
 
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static String[] createOPattern() {
+        return new String[]{
+                "     ***     ",
+                "   **   **   ",
+                "  **     **  ",
+                "  **     **  ",
+                "  **     **  ",
+                "   **   **   ",
+                "     ***     "
+        };
+    }
+
+    public static String[] createPPattern() {
+        return new String[]{
+                "  ******    ",
+                "  **    **  ",
+                "  **    **  ",
+                "  ******    ",
+                "  **        ",
+                "  **        ",
+                "  **        "
+        };
+    }
+
+    public static String[] createSPattern() {
+        return new String[]{
+                "    *****   ",
+                "  **     ** ",
+                "  **         ",
+                "    *****   ",
+                "        **   ",
+                "  **     ** ",
+                "    *****   "
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Step 1: Create String Array to store 7 lines
+        CharacterPattern[] letters = {
+                new CharacterPattern('O', createOPattern()),
+                new CharacterPattern('P', createPPattern()),
+                new CharacterPattern('S', createSPattern())
+        };
+
         String[] banner = new String[7];
 
-        // Step 2: Populate array using String.join()
+        for (int i = 0; i < 7; i++) {
 
-        banner[0] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                "  ****** ",
-                "  ****** ");
+            StringBuilder sb = new StringBuilder();
 
-        banner[1] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *       ");
+            sb.append(letters[0].getPattern()[i]).append(" ");
+            sb.append(letters[0].getPattern()[i]).append(" ");
+            sb.append(letters[1].getPattern()[i]).append(" ");
+            sb.append(letters[2].getPattern()[i]);
 
-        banner[2] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *       ");
+            banner[i] = sb.toString();
+        }
 
-        banner[3] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                "  ****** ",
-                "  *****  ");
-
-        banner[4] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
-
-        banner[5] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
-
-        banner[6] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " *       ",
-                "  *****  ");
-
-        // Step 3: Print using Enhanced For Loop
         for (String line : banner) {
             System.out.println(line);
         }
